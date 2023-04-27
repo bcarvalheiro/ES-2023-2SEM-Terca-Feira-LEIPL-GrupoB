90daa629d95f133d065c646d4e71520262a46a9e
Diogo <diogo_miguel10@hotmail.com>
2023-04-27 20:20:18 +0100

    Javadoc for CalendarUtils's functions

--- src/main/java/org/es/leipl/tercafeira/grupob/tools/gui/swingCalendar/CalendarUtils.java
+++ src/main/java/org/es/leipl/tercafeira/grupob/tools/gui/swingCalendar/CalendarUtils.java
@@ -1,6 +1,5 @@
 package org.es.leipl.tercafeira.grupob.tools.gui.swingCalendar;
 
-
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
@@ -12,18 +11,36 @@ public final class CalendarUtils {
 
     private CalendarUtils() {}
 
+    /**
+     * @return - The singleton instance
+     */
     public static CalendarUtils getInstance() {
         if (INSTANCE == null)
             INSTANCE = new CalendarUtils();
         return INSTANCE;
     }
 
-
+    /**
+     * Converts one calendar event to a String to be used on a CSV file
+     * @param event - A calendar event
+     * @param delimiter - The delimiter of the string
+     * @return - A String to be used on a CSV file
+     */
     public String calendarEventToString(CalendarEvent event, String delimiter) {
         return event.getDate() + delimiter + event.getStart() + delimiter + event.getEnd() + delimiter + event.getText();
     }
 
-    public void calendarEventsToCsvFile(List<CalendarEvent> events, String filePath, String delimiter) throws IOException {
+    /**
+     * Converts a list of calendar events to a CSV file
+     * @param events - The list of the calendar events
+     * @param filePath - The path to save the file
+     * @param delimiter - The delimiter of the csv
+     * @throws IOException - When the path doesn't exist
+     * @throws IllegalArgumentException - When at least one of the parameters is null
+     */
+    public void calendarEventsToCsvFile(List<CalendarEvent> events, String filePath, String delimiter) throws IOException, IllegalArgumentException {
+        if ( events == null || filePath == null || delimiter == null )
+            throw new IllegalArgumentException();
         FileWriter csvWriter = new FileWriter(filePath);
         csvWriter.append(HEADER);
         for (CalendarEvent event : events) {
