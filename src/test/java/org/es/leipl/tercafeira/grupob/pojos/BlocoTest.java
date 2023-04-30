4fa86a47110cfcf27163bf24906a69ad3735a273
CarolinaVRoque <carolinavarandaroque@gmail.com>
2023-04-30 17:18:00 +0100

    - Commit da classe teste BlocoTest.java, foram adicinados novos @Test à classe.

--- src/test/java/org/es/leipl/tercafeira/grupob/pojos/BlocoTest.java
+++ src/test/java/org/es/leipl/tercafeira/grupob/pojos/BlocoTest.java
@@ -4,14 +4,12 @@ import org.junit.Test;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.util.Calendar;
-import java.util.Date;
 
 import static org.junit.Assert.*;
 
 public class BlocoTest {
 
-    Bloco bloco = new Bloco("LEI,MEI", "POO", "TP1", "A", 30, "Segunda", LocalTime.of(13,00,00), LocalTime.of(14,30,00), LocalDate.of(2022,12,02), "Sala 1", 40);
+    Bloco bloco = new Bloco("LEI,MEI", "POO", "TP1", "A", 30, "Segunda", LocalTime.of(13, 00, 00), LocalTime.of(14, 30, 00), LocalDate.of(2022, 12, 02), "Sala 1", 40);
 
 
     @Test
@@ -31,5 +29,68 @@ public class BlocoTest {
 
         assertEquals(expected, actual);
 
+    }
+
+    @Test
+    public void testGetUC() {
+        Bloco bloco = new Bloco("LEI,MEI", "POO", "TP1", "A", 30, "Segunda", LocalTime.of(13, 00, 00), LocalTime.of(14, 30, 00), LocalDate.of(2022, 12, 02), "Sala 1", 40);
+        assertEquals("POO", bloco.getUc());
+    }
+
+    @Test
+    public void testGetTurno() {
+        Bloco bloco = new Bloco("LEI,MEI", "POO", "TP1", "A", 30, "Segunda", LocalTime.of(13, 00, 00), LocalTime.of(14, 30, 00), LocalDate.of(2022, 12, 02), "Sala 1", 40);
+        assertEquals("TP1", bloco.getTurno());
+    }
+
+    @Test
+    public void testGetTurma() {
+        Bloco bloco = new Bloco("LEI,MEI", "POO", "TP1", "A", 30, "Segunda", LocalTime.of(13, 00, 00), LocalTime.of(14, 30, 00), LocalDate.of(2022, 12, 02), "Sala 1", 40);
+        assertEquals("A", bloco.getTurma());
+    }
+
+    @Test
+    public void testGetInscritos() {
+        Bloco bloco = new Bloco("LEI,MEI", "POO", "TP1", "A", 30, "Segunda", LocalTime.of(13, 00, 00), LocalTime.of(14, 30, 00), LocalDate.of(2022, 12, 02), "Sala 1", 40);
+        assertEquals(30, bloco.getInscritos());
+    }
+
+    @Test
+    public void testGetDiaSemana() {
+        Bloco bloco = new Bloco("LEI,MEI", "POO", "TP1", "A", 30, "Segunda", LocalTime.of(13, 00, 00), LocalTime.of(14, 30, 00), LocalDate.of(2022, 12, 02), "Sala 1", 40);
+        assertEquals("Segunda", bloco.getDiaSemana());
+    }
+
+    @Test
+    public void testGetHoraInicio() {
+        Bloco bloco = new Bloco("LEI,MEI", "POO", "TP1", "A", 30, "Segunda", LocalTime.of(13, 00, 00), LocalTime.of(14, 30, 00), LocalDate.of(2022, 12, 02), "Sala 1", 40);
+        assertEquals(LocalTime.of(13, 00, 00), bloco.getHoraIni());
+    }
+
+    @Test
+    public void testGetHoraFim() {
+        Bloco bloco = new Bloco("LEI,MEI", "POO", "TP1", "A", 30, "Segunda", LocalTime.of(13, 00, 00), LocalTime.of(14, 30, 00), LocalDate.of(2022, 12, 02), "Sala 1", 40);
+        assertEquals(LocalTime.of(14, 30, 00), bloco.getHoraFim());
+    }
+
+    @Test
+    public void testGetData() {
+        assertEquals(LocalDate.of(2022, 12, 02), bloco.getData());
+    }
+
+    @Test
+    public void testGetSala() {
+        assertEquals("Sala 1", bloco.getSala());
+    }
+
+    @Test
+    public void testGetLotacao() {
+        assertEquals(40, bloco.getLotacao());
+    }
+
+    @Test
+    public void testSetHorarioIni() {
+        bloco.setHoraIni(LocalTime.of(14, 00, 00));
+        assertEquals(LocalTime.of(14, 00, 00), bloco.getHoraIni());
     }
 }
