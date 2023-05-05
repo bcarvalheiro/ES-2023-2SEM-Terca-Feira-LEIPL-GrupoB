36a5fe55f69c4736d83cfd7082ec7e0e82da5cec
neoeve <dadcl87@gmail.com>
2023-05-05 14:47:45 +0100

    Webcal  fix

--- src/main/java/org/es/leipl/tercafeira/grupob/Main.java
+++ src/main/java/org/es/leipl/tercafeira/grupob/Main.java
@@ -15,7 +15,8 @@ public class Main {
     public static void main(String[] args) throws IOException, ParserException, CsvValidationException {
         GUI.createGUI();
         GUI.showGUI();
-        String url ="https://fenix.iscte-iul.pt/publico/publicPersonICalendar.do?method=iCalendar&username=dadcl1@iscte.pt&password=7YMg7TEBxyjKL0saPhAR1FNmMUfXMSSR9kPCNo4nOOa84cRL7Fn9d277xDXbmAhdhbVGltb31RpnAx2AIw41ptVtHs5PlSx1Z9ASWrMVqZy3rqEsok5iVxwLewx72vQq";
+        //coloquem o vosso webcal link aqui, substituir webcal: por https://
+        String url ="";
 
         Webcal call = new Webcal(url);
         String result = call.startWebcall();
--- src/main/java/org/es/leipl/tercafeira/grupob/tools/Webcal.java
+++ src/main/java/org/es/leipl/tercafeira/grupob/tools/Webcal.java
@@ -3,13 +3,24 @@ package org.es.leipl.tercafeira.grupob.tools;
 import java.io.*;
 import java.net.*;
 
+/**
+ * @author GRUPO_B_LEI_PL
+ * @version 0.0
+ */
 public class Webcal {
 
+    /**
+     * Url to get webcal
+     */
     private String url;
     public Webcal(String url) {
         this.url = url;
     }
 
+    /**
+     * downloads the webcal calendar
+     * @return Sring ICS content
+     */
     public String startWebcall() throws IOException {
         URL webcalUrl = new URL(url);
         URLConnection connection = webcalUrl.openConnection();
